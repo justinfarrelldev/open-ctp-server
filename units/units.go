0bd0cebe3d2a989e35841b7f692d11f107c30b7a
Justin Farrell <justinfarrellwebdev@gmail.com>
2023-12-31 17:07:06 -0500

    Expanded Abolitionist information

--- units/units.go
+++ units/units.go
@@ -13,8 +13,36 @@ type Server struct {
 func (s *Server) GetUnit(ctx context.Context, unitInfo *UnitInfo) (*Unit, error) {
 	if *unitInfo.Type == UnitType_ABOLITIONIST {
 		description := "The Abolitionist is your primary weapon against the insidious attacks of foreign <L:DATABASE_UNITS,UNIT_SLAVER>Slavers<e>.  Two actions are at her disposal:  the <L:DATABASE_ORDERS,ORDER_UNDERGROUND_RAILWAY>Free Slave<e> action, which frees any slaves in the target city, and <L:DATABASE_ORDERS,ORDER_INCITE_UPRISING>Aid Uprising<e>, which can help propel a restless enemy city into full-scale <L:DATABASE_CONCEPTS,CONCEPT_RIOTS>Rioting<e>.\n\nBecause the Abolitionist is a <L:DATABASE_CONCEPTS,CONCEPT_STEALTH_UNITS>Stealth Unit<e>, it is able to see other stealth units."
+		var (
+			attack        int32 = 0
+			defense       int32 = 10
+			zbRangeAttack int32 = 0
+			firepower     int32 = 1
+			armor         int32 = 1
+			maxHp         int32 = 10
+			shieldCost    int32 = 540
+			powerPoints   int32 = 250
+			shieldHunger  int32 = 5
+			foodHunger    int32 = 0
+			maxMovePoints int32 = 300
+			visionRange   int32 = 1
+		)
+
 		return &Unit{
-			Description: &description,
+			Description:   &description,
+			Category:      UnitCategory.Enum(UnitCategory_SPECIAL),
+			Attack:        &attack,
+			Defense:       &defense,
+			ZbRangeAttack: &zbRangeAttack,
+			Firepower:     &firepower,
+			Armor:         &armor,
+			MaxHp:         &maxHp,
+			ShieldCost:    &shieldCost,
+			PowerPoints:   &powerPoints,
+			ShieldHunger:  &shieldHunger,
+			FoodHunger:    &foodHunger,
+			MaxMovePoints: &maxMovePoints,
+			VisionRange:   &visionRange,
 		}, nil
 	}
 
