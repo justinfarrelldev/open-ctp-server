b74caa32bd8a428692a5753deee5b77e33d4391e
Justin Farrell <justinfarrellwebdev@gmail.com>
2023-12-31 00:15:48 -0500

    Basic TCP Go listener established

--- src/main.go
+++ src/main.go
@@ -2,8 +2,27 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	port := 9000
+
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		log.Fatalf("Failed to listen on port %d", port)
+	}
+
+	fmt.Printf("Listening to TCP on port %d\n", port)
+
+	grpcServer := grpc.NewServer()
+
+	if err := grpcServer.Serve(listener); err != nil {
+		log.Fatalf("Failed to serve gRPC over port %d: %v", port, err)
+	}
 }
