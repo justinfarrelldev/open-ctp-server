157b5b97ef96a509ab7f511501dda157db166336
Justin Farrell <justinfarrellwebdev@gmail.com>
2024-10-29 22:38:29 -0400

    Rewrote code to only update what is specified (need to have it show the correct output, though)

--- internal/account/update_account.go
+++ internal/account/update_account.go
@@ -15,15 +15,15 @@ type UpdateAccountArgs struct {
 	// The account ID for the account that will be updated.
 	AccountId int64 `json:"account_id"`
 	// The new name for the account.
-	Name string `json:"name"`
+	Name *string `json:"name,omitempty"`
 	// The new info for the account.
-	Info string `json:"info"`
+	Info *string `json:"info,omitempty"`
 	// The new location for the account.
-	Location string `json:"location"`
+	Location *string `json:"location,omitempty"`
 	// The new email for the account.
-	Email string `json:"email"`
+	Email *string `json:"email,omitempty"`
 	// The new experience level for the account.
-	ExperienceLevel int `json:"experience_level"`
+	ExperienceLevel *int `json:"experience_level,omitempty"`
 }
 
 // UpdateAccount updates an account by the account ID.
@@ -58,20 +58,58 @@ func UpdateAccount(w http.ResponseWriter, r *http.Request, db *sql.DB) error {
 		return errors.New("an error occurred while decoding the request body:" + err.Error())
 	}
 
-	_, err = db.Exec("UPDATE account SET name = $1, info = $2, location = $3, email = $4, experience_level = $5 WHERE id = $6",
-		args.Name, args.Info, args.Location, args.Email, args.ExperienceLevel, args.AccountId)
+	if args.AccountId == 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		return errors.New("account_id must be specified")
+	}
 
+	query := "UPDATE account SET "
+	params := []interface{}{}
+	paramIndex := 1
+
+	if args.Name != nil {
+		query += fmt.Sprintf("name = $%d, ", paramIndex)
+		params = append(params, *args.Name)
+		paramIndex++
+	}
+	if args.Info != nil {
+		query += fmt.Sprintf("info = $%d, ", paramIndex)
+		params = append(params, *args.Info)
+		paramIndex++
+	}
+	if args.Location != nil {
+		query += fmt.Sprintf("location = $%d, ", paramIndex)
+		params = append(params, *args.Location)
+		paramIndex++
+	}
+	if args.Email != nil {
+		query += fmt.Sprintf("email = $%d, ", paramIndex)
+		params = append(params, *args.Email)
+		paramIndex++
+	}
+	if args.ExperienceLevel != nil {
+		query += fmt.Sprintf("experience_level = $%d, ", paramIndex)
+		params = append(params, *args.ExperienceLevel)
+		paramIndex++
+	}
+
+	// Remove the trailing comma and space
+	query = query[:len(query)-2]
+	query += fmt.Sprintf(" WHERE id = $%d", paramIndex)
+	params = append(params, args.AccountId)
+
+	_, err = db.Exec(query, params...)
 	if err != nil {
 		return fmt.Errorf("an error occurred while updating the account with the ID %d: %v", args.AccountId, err)
 	}
 
 	// Now assemble the variables into the Account struct
 	account := Account{
-		Name:            args.Name,
-		Info:            args.Info,
-		Location:        args.Location,
-		Email:           args.Email,
-		ExperienceLevel: ExperienceLevel(args.ExperienceLevel),
+		Name:            getStringValue(args.Name),
+		Info:            getStringValue(args.Info),
+		Location:        getStringValue(args.Location),
+		Email:           getStringValue(args.Email),
+		ExperienceLevel: ExperienceLevel(getIntValue(args.ExperienceLevel)),
 	}
 
 	accountBytes, err := json.Marshal(account)
@@ -83,4 +121,18 @@ func UpdateAccount(w http.ResponseWriter, r *http.Request, db *sql.DB) error {
 
 	fmt.Println("Successfully updated account!")
 	return nil
+}
+
+func getStringValue(s *string) string {
+	if s == nil {
+		return ""
+	}
+	return *s
+}
+
+func getIntValue(i *int) int {
+	if i == nil {
+		return 0
+	}
+	return *i
 }
