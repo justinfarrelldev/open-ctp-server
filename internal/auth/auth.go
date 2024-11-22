b6accbdb66c9dfe6dbd9ae7635af8026b04897c0
Justin Farrell <justinfarrellwebdev@gmail.com>
2024-11-22 10:26:25 -0500

    Changed unnecessarily exported code to only be private in auth.go

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -10,15 +10,15 @@ import (
 	"golang.org/x/crypto/argon2"
 )
 
-// HashSalt represents a salt and a hash in the same data type for password storage.
+// hashSalt represents a salt and a hash in the same data type for password storage.
 //
 // @Description Structure containing both a salt and a hash for password storage.
-type HashSalt struct {
+type hashSalt struct {
 	hash []byte
 	salt []byte
 }
 
-type Argon2idHash struct {
+type argon2idHash struct {
 	// time represents the number of
 	// passed over the specified memory.
 	time uint32
@@ -39,8 +39,8 @@ type Argon2idHash struct {
 
 // NewArgon2idHash constructor function for
 // Argon2idHash.
-func NewArgon2idHash(time, saltLen uint32, memory uint32, threads uint8, keyLen uint32) *Argon2idHash {
-	return &Argon2idHash{
+func NewArgon2idHash(time, saltLen uint32, memory uint32, threads uint8, keyLen uint32) *argon2idHash {
+	return &argon2idHash{
 		time:    time,
 		saltLen: saltLen,
 		memory:  memory,
@@ -62,7 +62,7 @@ func randomSecret(length uint32) ([]byte, error) {
 // GenerateHash using the password and provided salt.
 // If not salt value provided fallback to random value
 // generated of a given length.
-func (a *Argon2idHash) GenerateHash(password, salt []byte) (*HashSalt, error) {
+func (a *argon2idHash) GenerateHash(password, salt []byte) (*hashSalt, error) {
 	var err error
 
 	// If salt is not provided generate a salt of
@@ -79,12 +79,12 @@ func (a *Argon2idHash) GenerateHash(password, salt []byte) (*HashSalt, error) {
 	hash := argon2.IDKey(password, salt, a.time, a.memory, a.threads, a.keyLen)
 
 	// Return the generated hash and salt used for storage.
-	return &HashSalt{hash: hash, salt: salt}, nil
+	return &hashSalt{hash: hash, salt: salt}, nil
 
 }
 
 // Compare generated hash with store hash.
-func (a *Argon2idHash) Compare(hash, salt, password []byte) error {
+func (a *argon2idHash) Compare(hash, salt, password []byte) error {
 	// Generate hash for comparison.
 	hashSalt, err := a.GenerateHash(password, salt)
 
@@ -101,7 +101,7 @@ func (a *Argon2idHash) Compare(hash, salt, password []byte) error {
 	return nil
 }
 
-func StoreHashAndSalt(hashSalt *HashSalt, accountEmail string, db *sql.DB) error {
+func StoreHashAndSalt(hashSalt *hashSalt, accountEmail string, db *sql.DB) error {
 	result, err := db.Query("INSERT INTO passwords (account_email, hash, salt) VALUES ($1, $2, $3)", accountEmail, base64.StdEncoding.EncodeToString(hashSalt.hash), base64.StdEncoding.EncodeToString(hashSalt.salt))
 	if err != nil {
 		return errors.New("an error occurred while inserting a hash-salt pair into the database: " + err.Error())
